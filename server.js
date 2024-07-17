e20f503be18171d3d4f735d3579b8af807c03d57
Ralf Siewert <ralf@actyvy.st>
2024-07-17 11:50:08 +0200

    add POST /notes route (half way)

--- server.js
+++ server.js
@@ -1,13 +1,58 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const notes = [
+  {
+    id: 34,
+    text: "Küche aufräumen",
+  },
+  {
+    id: 5,
+    text: "Einkaufen",
+  },
+  {
+    id: 17,
+    text: "Wäsche waschen",
+  },
+];
+
+function getNextId() {
+  // iterate over the array and find the highest id used. return this number +1
+}
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello world from Express");
+  res.json({ msg: "Hello from the Notes App" });
 });
 
 app.get("/notes", (req, res) => {
-  res.send("Hello world from the /notes route!");
+  res.json(notes);
+});
+
+app.get("/notes/:id", (req, res) => {
+  const { id } = req.params;
+
+  const note = notes.find((note) => note.id === Number(id));
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).json({ error: "resource not found" });
+  }
+});
+
+app.post("/notes", (req, res) => {
+  console.log(req.body);
+  const newNote = req.body;
+  const nextId = getNextId();
+  notes.push({
+    ...newNote, //spread operator
+    id: nextId,
+  });
+  console.log(notes);
+  res.json({ msg: "This is the POST /notes route" });
 });
 
 app.listen(3000, () => {
